c1e37d46c5ec728a801a3a17282466eef1853d5a
Sebastian Mack <mack.seb@gmail.com>
2018-06-08 15:23:32 +0200

    change text

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         NEW <code>src/App.js</code> and save to reload.
         </p>
         <Button type="primary">Button</Button>
       </div>
